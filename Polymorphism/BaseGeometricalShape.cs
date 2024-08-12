d95f992d71bdd00fb7ed1611193e6e52c4cc4d7e
sinan <sinan@SINAN-SENKUL>
2024-08-12 03:28:09 +0300

    Add project files.

---
+++ Polymorphism/BaseGeometricalShape.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class BaseGeometricShape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public virtual void calcArea()
+    {
+        int area = Width * Height;
+        Console.WriteLine($"area is: {area}");
+    }
+}
+
+public class Square : BaseGeometricShape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Square(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+    public override void calcArea()
+    {
+        int area = Width * Height;
+        Console.WriteLine($"area is: {area}");
+    }
+}
+
+public class Rectangle : BaseGeometricShape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+    public override void calcArea()
+    {
+        int area = Width * Height;
+        Console.WriteLine($"area is: {area}");
+    }
+}
+
+public class RightTriangle : BaseGeometricShape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public RightTriangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+    public override void calcArea()
+    {
+        int area = (Width * Height) / 2;
+        Console.WriteLine($"area is: {area}");
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main()
+        {
+            BaseGeometricShape square = new Square(4, 2);
+            BaseGeometricShape rectangle = new Rectangle(5, 3);
+            BaseGeometricShape triangle = new RightTriangle(6, 4);
+
+            square.calcArea();
+            rectangle.calcArea();
+            triangle.calcArea();
+        }
+    }
+}
